ca82e38ed68e37511a32fcbfca5afcd2225cba50
Viachaslau Kitsun <Viachaslau_Kitsun@epam.com>
2019-02-27 09:28:20 +0300

    UnitTests
    added some UnitTests

---
+++ Task3/StringParser.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringParser.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("StringParser.Tests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19a37130-1a6f-45ad-a8af-03b14e4315f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task3/StringParser.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringParser.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase("-1")]
+        
+        public void TestCorrectParsing(string s)
+        {
+
+        }
+    }
+}
